d7fa9f7d8293e9403cb8a4e7b48df36083e8b23c
aktheraja <wale.akin2007@gmail.com>
2019-09-06 10:02:37 -0600

    updated

--- src/App.js
+++ src/App.js
@@ -3,21 +3,31 @@ import './App.css';
 import Nav from './Components/NAV/Nav';
 import Form from './Components/Forms/Forms';
 import Carousel from './Components/Jumbotron/Jumbotron';
+import Modal from './Components/Modal/Modal';
 
 class App extends Component {
   state = {
-    name:null
-  }
+    name:null,
+      show:false
+  };
   nameHandler =(event)=>{
     console.log(event.target.value);
     this.setState({name:event.target.value})
 
-  }
+  };
+    handleClose = () => {
+        this.setState({show:false});
+    };
+    handleShow = () => {
+        this.setState({show:true});
+    };
+
   render() {
     return (
       <div className="container">
         <Nav/>
-        <Carousel/>
+        <Modal show={this.state.show} handleClose={this.handleClose}/>
+        <Carousel showModal={this.handleShow}/>
         <Form/>
       </div>
 
--- src/Components/Jumbotron/Jumbotron.js
+++ src/Components/Jumbotron/Jumbotron.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {Jumbotron,Button} from  "react-bootstrap";
 import classes from './Jumbotron.css';
-const jumbotron =()=>{
+const jumbotron =(props)=>{
     return(
         <div className="container " >
             <Jumbotron className={classes.jumbotron}>
                 <h1>Dr. Charles Osime's Send Forth!</h1>
-                    <Button variant="primary">Click here to Learn more</Button>
+                    <Button variant="primary" onClick ={props.showModal}>Click here to Learn more</Button>
             </Jumbotron>
         </div>
     );
-}
+};
 export default jumbotron;
